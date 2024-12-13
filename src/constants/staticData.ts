ee036a7b8fe9d2b56fb301ff466810abc6717ffc
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2024-12-13 12:49:35 +0530

    implement event card
    Implement the cards in the Near you and Events recommendations sections with the metadata and the image available

---
+++ src/constants/staticData.ts
@@ -0,0 +1,60 @@
+export const nearYou = [
+  {
+    title: 'Movie Night',
+    location: 'Lower Manhattan',
+    topLabel: '$10',
+    time: 'Sat.9:00 PM',
+    cover:
+      'https://raw.githubusercontent.com/gaurav712/lh-demo/refs/heads/main/src/assets/images/movie-night.jpeg',
+  },
+  {
+    title: 'Cricket Match',
+    location: 'Lower Manhattan',
+    topLabel: '11/21',
+    time: 'Sun.10:00 PM',
+    cover:
+      'https://raw.githubusercontent.com/gaurav712/lh-demo/refs/heads/main/src/assets/images/cricket-match.jpeg',
+  },
+];
+
+export const eventRecommendations = [
+  {
+    title: 'GG Night: Game & Chill Session',
+    location: 'Lower Manhattan',
+    topLabel: '$10',
+    time: 'Sat.9:00 PM',
+    cover:
+      'https://raw.githubusercontent.com/gaurav712/lh-demo/refs/heads/main/src/assets/images/game-night.jpeg',
+  },
+  {
+    title: 'GG Night: Game & Chill Session',
+    location: 'Lower Manhattan',
+    topLabel: '$10',
+    time: 'Sat.9:00 PM',
+    cover:
+      'https://raw.githubusercontent.com/gaurav712/lh-demo/refs/heads/main/src/assets/images/game-night-alt.jpeg',
+  },
+];
+
+export const connectionRecommendations = [
+  {
+    name: 'Ava Martinez',
+    location: 'Upper West Side',
+    cover: 'ava-martinez.png',
+  },
+  {
+    name: 'Emma Johnson',
+    location: 'Upper West Side',
+    cover: 'emma-johnson.png',
+  },
+  {
+    name: 'Claude Kuhic',
+    location: 'Upper West Side',
+    cover: 'claude-kuhic.png',
+  },
+  {
+    name: 'William Blick',
+    location: 'Upper West Side',
+    cover: 'william-blick.png',
+  },
+];
--- src/screens/Explore.tsx
+++ src/screens/Explore.tsx
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react';
 import {
+  ImageBackground,
   ScrollView,
   StyleSheet,
   Text,
@@ -13,6 +14,45 @@ import SidebarIcon from '../assets/icons/SidebarIcon';
 import {NavigationContext} from '../context/NavigationContext';
 import ExploreIcon from '../assets/icons/ExploreIcon';
 import {FlatList} from 'react-native-gesture-handler';
+import {eventRecommendations, nearYou} from '../constants/staticData';
+import {BlurView} from '@react-native-community/blur';
+
+interface IEvent {
+  title: string;
+  location: string;
+  topLabel: string;
+  time: string;
+  cover: string;
+}
+
+const renderEvent = ({item}: {item: IEvent}) => (
+  <ImageBackground source={{uri: item.cover}} style={styles.eventCard}>
+    <View style={styles.topLabelContainer}>
+      <BlurView style={styles.topLabel}>
+        <View>
+          <Text style={styles.label}>{item.topLabel}</Text>
+        </View>
+      </BlurView>
+    </View>
+    <View style={styles.timeLabelContainer}>
+      <BlurView style={styles.timeLabel}>
+        <View>
+          <Text style={styles.label}>{`🗓️ ${item.time}`}</Text>
+        </View>
+      </BlurView>
+    </View>
+    <View style={styles.eventInfoContainer}>
+      <BlurView style={styles.blurContainer}>
+        <View>
+          <Text style={styles.eventTitle}>{item.title}</Text>
+        </View>
+        <View>
+          <Text style={styles.eventLocation}>{item.location}</Text>
+        </View>
+      </BlurView>
+    </View>
+  </ImageBackground>
+);
 
 const Explore = () => {
   const {openSidebar} = useContext(SidebarContext);
@@ -42,8 +82,32 @@ const Explore = () => {
           placeholderTextColor={'#7F7F7F'}
         />
       </View>
-      <ScrollView style={styles.bottomContentContainer}>
-        <FlatList data={[]} renderItem={() => <></>} />
+      <ScrollView
+        style={styles.bottomContentContainer}
+        contentContainerStyle={styles.bottomContent}
+        showsVerticalScrollIndicator={false}>
+        <Text style={styles.sectionHeading}>
+          Near you<Text style={styles.count}>{` (${nearYou.length})`}</Text>
+        </Text>
+        <FlatList
+          style={styles.eventsList}
+          data={nearYou}
+          renderItem={renderEvent}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        />
+        <Text style={styles.sectionHeading}>
+          Events you may like
+          <Text
+            style={styles.count}>{` (${eventRecommendations.length})`}</Text>
+        </Text>
+        <FlatList
+          style={styles.eventsList}
+          data={eventRecommendations}
+          renderItem={renderEvent}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        />
       </ScrollView>
     </View>
   );
@@ -107,6 +171,88 @@ const styles = StyleSheet.create({
     borderColor: '#9C9C9C',
     borderRadius: 15,
     marginHorizontal: -0.2,
+    paddingVertical: 20,
+  },
+  bottomContent: {
+    paddingBottom: 140,
+  },
+  sectionHeading: {
+    color: '#fff',
+    fontSize: 20,
+    fontFamily: 'SF-Pro-Display-Semibold',
+    marginBottom: 15,
+    paddingHorizontal: 20,
+  },
+  count: {
+    color: '#7F7F7F',
+  },
+  eventsList: {
+    paddingHorizontal: 20,
+  },
+  eventCard: {
+    width: 260,
+    aspectRatio: 274 / 311,
+    marginRight: 20,
+    borderRadius: 20,
+    overflow: 'hidden',
+  },
+  topLabelContainer: {
+    position: 'absolute',
+    overflow: 'hidden',
+    left: 10,
+    top: 10,
+    borderRadius: 10,
+  },
+  topLabel: {
+    backgroundColor: '#4242424D',
+    alignSelf: 'flex-start',
+    paddingHorizontal: 10,
+  },
+  label: {
+    textAlign: 'center',
+    fontFamily: 'SF-Pro-Display-Regular',
+    color: '#fff',
+  },
+  timeLabelContainer: {
+    position: 'absolute',
+    overflow: 'hidden',
+    right: 10,
+    bottom: 80,
+    borderRadius: 10,
+  },
+  timeLabel: {
+    backgroundColor: '#4242424D',
+    alignSelf: 'flex-start',
+    paddingHorizontal: 10,
+  },
+  eventInfoContainer: {
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    left: 0,
+  },
+  blurContainer: {
+    position: 'relative',
+    width: '100%',
+    paddingHorizontal: 10,
+  },
+  eventTitle: {
+    maxWidth: '100%',
+    color: '#fff',
+    fontFamily: 'SF-Pro-Display-Semibold',
+    fontSize: 16,
+    includeFontPadding: false,
+    marginBottom: 2,
+    marginTop: 15,
+  },
+  eventLocation: {
+    maxWidth: '100%',
+    color: '#7F7F7F',
+    fontFamily: 'SF-Pro-Display-Regular',
+    fontSize: 16,
+    includeFontPadding: false,
+    marginBottom: 15,
   },
 });
 
